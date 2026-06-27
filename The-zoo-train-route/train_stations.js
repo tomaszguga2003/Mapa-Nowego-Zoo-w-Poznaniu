26c3c04b1e068fd073af95d1e46a197a7db8ee70
tomaszguga2003 <tomaszguga3@gmail.com>
2026-06-27 14:29:02 +0200

    Aktualizacja 27.06.2026 - wtyczki, zdjęcia, sygnatury i inne

---
+++ The-zoo-train-route/train_stations.js
@@ -0,0 +1,12 @@
+var train_stations = {
+"type": "FeatureCollection",
+"name": "train_stations",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "numer": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 16.995347949436724, 52.400275875782242 ] } },
+{ "type": "Feature", "properties": { "id": 2, "numer": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 16.998923571954059, 52.397091540242386 ] } },
+{ "type": "Feature", "properties": { "id": 3, "numer": 2.0 }, "geometry": { "type": "Point", "coordinates": [ 17.011150890230013, 52.400662056800662 ] } },
+{ "type": "Feature", "properties": { "id": 4, "numer": 3.0 }, "geometry": { "type": "Point", "coordinates": [ 17.002110349794911, 52.400675875002307 ] } },
+{ "type": "Feature", "properties": { "id": 5, "numer": 4.0 }, "geometry": { "type": "Point", "coordinates": [ 17.000745431281977, 52.402211258738475 ] } }
+]
+}
